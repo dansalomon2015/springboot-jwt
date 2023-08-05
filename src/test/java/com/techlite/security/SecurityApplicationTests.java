18d0d0f29dd5f1c029a7a4dfa8484912fff94ba0
Salomon Nghukam <dsalomonj@gmail.com>
2023-08-05 23:29:55 +0100

    first commit

---
+++ src/test/java/com/techlite/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlite.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
